1450b289dc03a3ec77522797ff33a503714177bd
Gabriel Viza <vmgronaldo@gmail.com>
2021-01-19 18:10:48 -0500

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Verificar Certiciados | Lidera EHSQ</title>
+        <title>Verificar Certificados | Lidera EHSQ</title>
 
         <!-- Fonts -->
         <link rel="dns-prefetch" href="//fonts.gstatic.com">
